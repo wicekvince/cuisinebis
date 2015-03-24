4e2ae48c99e5b9e46867b6be5aa0431a02f42ff4
Tim59 <xx--xx@hotmail.fr>
2015-03-24 16:33:16 +0100

    modif views.py

--- recette/views.py
+++ recette/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .models import Ingredient,  Etape,  Photo,  Recette
+
 
 def index(request):
     contexte = {
@@ -6,3 +10,30 @@ def index(request):
     }
     return render(request, 'recette/index.html', contexte)
 
+
+def register(request):
+    if (request.method == 'POST'):
+        user_form = UserCreationForm(request.POST    )
+        if user_form.is_valid():
+            user = User.objects.create_user(request.POST['username'],'',request.POST['password1'])
+            user.save()
+            return redirect('recette:index')
+    else:
+        user_form = UserCreationForm()
+    contexte = {
+        'formulaire': user_form,
+    }
+    return render(request, 'registration/register.html', contexte)
+
+def recette(request, id):
+    recette = Recette.objects.get(id=id)
+    etapes = Etape.objects.filter(recette=id)
+    ingredients = Ingredient.objects.filter(recette=id)
+    photos = Photo.objects.filter(recette=id)
+    contexte = {
+        'recette'    : recette,
+        'etapes'     : etapes,
+        'ingredients': ingredients,
+        'photos'     : photos,
+    }
+    return render(request, 'recette/affiche-recette.html', contexte)
