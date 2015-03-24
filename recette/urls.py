d7907d08dbd2dd98c35164b9f700f69f8611eb11
Tim59 <xx--xx@hotmail.fr>
2015-03-24 17:18:13 +0100

    modif urls.py

--- recette/urls.py
+++ recette/urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls import patterns, include, url
-from .views import index
-from django.contrib import admin
-from django.contrib.auth.decorators import login_required
+from .views import index, register
+from .views import index, recette
 
 urlpatterns = patterns('',
     url(r'^$', index, name='index'),
-
+    url(r'^recette/(?P<id>\d+)/', recette, name="recette"),
+    url(r'^accounts/register/$', register, name='register'),
 )
+
+
+
+
